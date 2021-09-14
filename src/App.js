5062c7f92d8379f18662a54bbe53759d442d734d
Amanda Lin <amanda.lin.aut@gmail.com>
2021-09-14 08:57:34 -0400

    Container files routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 import './App.css';
+import Article from "./containers/Article";
+import ArticleListing from "./containers/ArticleListing";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        hi
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/article">
+          <Article />
+        </Route>
+
+        <Route path="/">
+          <ArticleListing />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
